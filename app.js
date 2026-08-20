5035a205a65a8eb8624a5836219a524873d8666f
Giga50 <djdhhfjfjf887@gmail.com>
2026-08-20 19:17:05 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+// NeoGram Supabase code
+// Здесь будут: регистрация, вход, аватарки, сообщения
+
+const SUPABASE_URL = "https://kbhjuliwskwfuboogcxr.supabase.co";
+const SUPABASE_KEY = "ВСТАВЬ_НОВЫЙ_ANON_KEY_СЮДА";
