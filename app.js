af1d13c5a094f107827e1e927aeee4a7d957a29f
Giga50 <djdhhfjfjf887@gmail.com>
2026-08-20 19:29:42 +0500

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-// NeoGram Supabase code
-// Здесь будут: регистрация, вход, аватарки, сообщения
-
-const SUPABASE_URL = "https://kbhjuliwskwfuboogcxr.supabase.co";
-const SUPABASE_KEY = "ВСТАВЬ_НОВЫЙ_ANON_KEY_СЮДА";
